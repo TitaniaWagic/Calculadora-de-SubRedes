2e819d8ce5584356b10ae72c8daa8ab5680f012d
TitaniaWagic <142698228+TitaniaWagic@users.noreply.github.com>
2025-05-09 19:19:37 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -3,6 +3,11 @@ import ipaddress
 import math
 import os
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app = Flask(__name__, static_url_path='/static')
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
 app = Flask(__name__, static_url_path='/static')
 
 def obtener_clase(ip):
