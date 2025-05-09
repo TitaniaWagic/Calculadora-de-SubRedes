990e4337f8bc5c298d8f702f7dde61292bb637da
TitaniaWagic <142698228+TitaniaWagic@users.noreply.github.com>
2025-05-09 19:21:51 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -8,8 +8,6 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__, static_url_path='/static')
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
-app = Flask(__name__, static_url_path='/static')
-
 def obtener_clase(ip):
     """Determina la clase de red según la dirección IP ingresada."""
     try:
